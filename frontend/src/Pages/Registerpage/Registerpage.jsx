091e10f3ff03e5682502619327912db6fb5b2823
Sumanth <sumanthpoojary965@gmail.com>
2025-07-10 19:43:52 +0530

    Update Registerpage.jsx

--- frontend/src/Pages/Registerpage/Registerpage.jsx
+++ frontend/src/Pages/Registerpage/Registerpage.jsx
@@ -19,7 +19,7 @@ export default function Registerpage() {
     }
 
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/user/', {
+      const response = await axios.post('https://foodifyy-backend-g4fx.onrender.com', {
         email: email,
         password: password,
         name: name
